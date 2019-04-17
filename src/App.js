3e59d489421c889edea16a1700c9cb4494d07505
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-17 18:06:17 -0400

    useHooks to get web3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const getWeb3 = require("@drizzle-utils/get-web3");
+const createDrizzleUtils = require("@drizzle-utils/core");
+
+function App() {
+  const initialState = {
+    web3: null,
+    drizzleUtils: null,
+    accounts: null
+  };
+
+  const [state, setAppState] = useState(initialState);
+
+  useEffect(() => {
+    const setup = async () => {
+      const web3 = await getWeb3();
+      const drizzleUtils = await createDrizzleUtils({ web3 });
+      const accounts = await drizzleUtils.getAccounts();
+
+      console.log(`Accounts: ${accounts}`);
+      setAppState({ ...state, web3, drizzleUtils, accounts });
+    };
+
+    setup();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
