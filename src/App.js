0006565c8be214ec395896621ed92fcded6dfcf7
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-17 18:12:01 -0400

    Move Layout one layer below app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import FrontPage from './components/frontPage';
 import "./App.css";
 
 const getWeb3 = require("@drizzle-utils/get-web3");
@@ -29,22 +29,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FrontPage />
   );
 }
 
