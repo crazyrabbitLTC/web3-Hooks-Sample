06666828f04f26ef78f84b4765fe4fe6fbaa88fc
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-17 18:36:50 -0400

    Subscription to ACcounts does not work.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import FrontPage from './components/frontPage';
+import FrontPage from "./components/frontPage";
 import "./App.css";
+import createCurrentAccount$ from "@drizzle-utils/current-account-stream";
 
 const getWeb3 = require("@drizzle-utils/get-web3");
 const createDrizzleUtils = require("@drizzle-utils/core");
@@ -9,7 +10,7 @@ function App() {
   const initialState = {
     web3: null,
     drizzleUtils: null,
-    accounts: null
+    currentAccount: null
   };
 
   const [state, setAppState] = useState(initialState);
@@ -19,18 +20,40 @@ function App() {
     const setup = async () => {
       const web3 = await getWeb3();
       const drizzleUtils = await createDrizzleUtils({ web3 });
-      const accounts = await drizzleUtils.getAccounts();
 
-      console.log(`Accounts: ${accounts}`);
-      setAppState({ ...state, web3, drizzleUtils, accounts });
+      setAppState({ ...state, web3, drizzleUtils });
     };
 
     setup();
   }, []);
 
-  return (
-    <FrontPage />
-  );
+
+
+  return <FrontPage />;
 }
 
 export default App;
+
+
+
+
+// Trying to subscribe to Accounts
+
+
+  // useEffect(() => {
+  //   const getAccounts = async () => {
+  //     if (state.web3) {
+  //       const web3 = await getWeb3();
+  //       console.log(web3);
+  //       const currentAccount$ = await createCurrentAccount$({web3});
+  //       const unsubscribe = currentAccount$.subscribe(currentAccount => {
+  //         console.log(currentAccount);
+  //         setAppState({ ...state, currentAccount });
+  //       });
+
+  //       return unsubscribe;
+  //     }
+  //   };
+
+  //   getAccounts();
+  // }, [state.web3]);
